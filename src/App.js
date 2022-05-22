8957e11ef16fc932e5c61a8a0789497c6445d551
Okoyedennis <okoyedennis7@gmail.com>
2022-05-22 18:10:51 +0100

    github finder application

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+const baseURL =
+  "https://api.github.com/search/repositories?q=created:%3E2021-08-13&sort=stars&order=desc";
 function App() {
+  const [request, setRequest] = useState([])
+
+  useEffect(() => {
+    axios.get(
+      baseURL
+    ).then((resp) => {
+      setRequest(resp.data.items)
+    });
+  }, [])
+  console.log(request);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {request.map((item) => (
+        <div className="app" key={item.id}>
+          <div className="logo">
+            <img src={item.owner.avatar_url} alt="" />
+          </div>
+          <div className="app__content">
+            <h2>{item.name}</h2>
+            <p>{item.description}</p>
+            <div className="app__content-bottom">
+              <div className="stars">{item.stargazers_count}</div>
+              <div className="issue">{item.open_issues}</div>
+              <p>Submited 30 days ago by {item.full_name}</p>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
